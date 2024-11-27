7b0e4b25720bc2b8f368a175508e7c99ce4047f2
Ckomer <stefangolijanin20@gmail.com>
2024-11-27 23:09:50 +0100

    Add files via upload

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,32 +1,36 @@
-// Importovanje potrebnih funkcionalnosti
 const { signature } = require("./lib/Sign");
 const { transactionState } = require("./lib/transaction-state");
 const { reversal } = require("./lib/reversal");
 
-// Definisanje CORS-a (ako je potrebno)
 const handleCORS = (response) => {
-  response.setHeader('Access-Control-Allow-Origin', 'https://jankos-wondrous-site-79299d.webflow.io/');
+  response.setHeader('Access-Control-Allow-Origin', '*'); // Dozvoljava pristup sa svih domena (ili zameniti '*' sa Webflow domenom, npr. 'https://jankos-wondrous-site-79299d.webflow.io')
   response.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   return response;
 };
 
-// Exportovanje funkcije kao handler
 exports.handler = async function(event, context) {
-  // Podesite CORS zaglavlja
+  // Dodajte CORS header-e za preflight (OPTIONS) i ostale HTTP metode
+  if (event.httpMethod === 'OPTIONS') {
+    const response = {
+      statusCode: 204,
+      body: JSON.stringify({ message: 'Preflight request allowed' })
+    };
+    return handleCORS(response);
+  }
+
+  // Ostatak koda za obradu POST zahteva
   const response = {
     statusCode: 200,
     body: JSON.stringify({ message: 'Success' })
   };
-  
-  // Dodajte CORS header-e ako je potrebno
+
   handleCORS(response);
 
-  // Možete obraditi zahteve ovde, npr. za različite rute kao:
+  // Obrada POST zahteva sa različitim akcijama
   if (event.httpMethod === 'POST') {
     const body = JSON.parse(event.body);
 
-    // Logika za obradu POST zahteva sa različitim funkcijama
     if (body.action === 'signature') {
       const signedData = await signature(body.params);
       response.body = JSON.stringify({ signedData });
