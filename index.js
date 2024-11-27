e509e7b495e81f4b9873c504f7d95da1e58e6326
Ckomer <stefangolijanin20@gmail.com>
2024-11-27 23:57:22 +0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-const signature = require("./lib/Sign");
-const { transactionState } = require("./lib/transaction-state");
-const { reversal } = require("./lib/reversal");
-
-module.exports = { signature, transactionState, reversal };
