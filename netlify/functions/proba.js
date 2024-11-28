350a8a19d8c5a74c89c91743b9b66b755f0eac64
Ckomer <stefangolijanin20@gmail.com>
2024-11-28 05:06:29 +0100

    Add files via upload

--- netlify/functions/proba.js
+++ netlify/functions/proba.js
@@ -24,22 +24,17 @@ app.use((req, res, next) => {
 // POST ruter direktno na '/.netlify/functions/proba'
 app.post('/.netlify/functions/proba', (req, res) => {
     // Privatni ključ za potpisivanje (RSA)
-    const privateKey = `-----BEGIN PRIVATE KEY-----
-    MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAOwmLx4+3ofOcvAa
-    FEmyhJmPXoYYN4IxJYfys9MJ2i2BW9lCn14Ph7sOWS7GQNAQLU0/3QHi+HCuL9Ub
-    Cw4OjcJ2t8+LJYUPBVIglg7wbpA20VxJyMzctpXqFwp/VnhD8677Ed9P5eOX4A3Q
-    cONB3LDmI56gP0vt4uaW793SCfJ/AgMBAAECgYA35socpNFU4MBAqkvHLmVJXLEJ
-    mU36mdvClOeQu7JUN10gzy6eu5pr43elJPVOy6WNteKBgdNCCVn1XpN9XmCcBcf8
-    /9l03T1IduxrRBcOxqycP1WVe3Yn7YgEO3cJUmouAS9SX7JekIEhwKnFlFLjf8xE
-    D2aMSyQZTo0IewsOsQJBAPiNkGCVJfMIm+N/KTtwahQx/TYXH/EOrzdIcxv5AV8/
-    q7k9hLQTbt7mdRmRTOamDrjulQlbclYIXDnmYcwcN7sCQQDzOXsiZ6J9JNkFEdhE
-    QEWGZim2f3cb+jgnVu2qRDReluXL485uM1BW5Y9P4W50a2hHaj1gMefjbD0o9B1n
-    ZHoNAkAzMxYWjyKuGYvjJQKdV97CIjoCtmjCA0BcpvPvQy/hKwB4vA9l+MrR568f
-    gSrSKErIFX1l3AyFZFt7IdXrcbSZAkAdN/2L34lMoV2U11T0tgfkImfGbb+gU8nl
-    J/M7LiBgcJ5AhShqYq23ErLJtNoxECoh0ih3YudpkbozLkhOAGsNAkEAkKNZKB/s
-    uE/2jQNLbbcxXJxO/5fsVglxmmYI8cYGcuG32XZTfi8NLobHVyLePLuxq+FwWnpp
-    1f8cvJr4PqL/IQ==
-    -----END PRIVATE KEY-----`; 
+    const fs = require('fs');
+    const path = require('path');
+
+    // Putanja do privatnog ključa
+    const privateKeyPath = path.join(__dirname, '..', 'keys', '1770000.pem');
+    const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+
+    // Korišćenje privatnog ključa
+    console.log(privateKey); // Samo za testiranje
+
+
 
     // Prikazivanje podataka za logovanje
     console.log('Received POST request with body:', req.body);
